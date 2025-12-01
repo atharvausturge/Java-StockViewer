af8fc10f81921177df96ef80740d74e85d6c29ee
atharvaru <160043531+atharvaru@users.noreply.github.com>
2025-12-01 12:31:10 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        frame.setTitle("Stock Viewer");
+        frame.setSize(1080,720);
+        frame.setVisible(true);
+    }
+}
